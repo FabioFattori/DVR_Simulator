2b6b51b39593ce08760b929bdddba414f9c07737
Fabio Fattori <fabio.fattori3@studio.unibo.it>
2024-11-27 16:01:24 +0100

    bug fix

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 
 
-# lista dei file presenti nella cartella NetworksDefaultTypes
-files = os.listdir("NetworksDefaultTypes")
-print("Lista dei file presenti nella cartella NetworksDefaultTypes:")
-for i in range(len(files)):
-    print(str(i) + " - " + files[i])
+while True:
+    # lista dei file presenti nella cartella NetworksDefaultTypes
+    files = os.listdir("NetworksDefaultTypes")
+    print("Lista dei file presenti nella cartella NetworksDefaultTypes:")
+    for i in range(len(files)):
+        print(str(i) + " - " + files[i])
 
-# chiedo all'utente quale file vuole runnare
-index = int(input("Quale rete si vuole creare? "))
-os.system("python NetworksDefaultTypes/" + files[index])
+    # chiedo all'utente quale file vuole runnare
+    index = int(input("Quale rete si vuole creare? "))
+    os.system("python NetworksDefaultTypes/" + files[index])
